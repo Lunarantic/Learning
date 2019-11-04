c536e5216231ff50d0315088afe2d95425ee87b5
Lunarantic <26676464+Lunarantic@users.noreply.github.com>
2019-11-04 14:01:54 -0500

    Created Program3.java

---
+++ src/exercise3/Program3.java
@@ -0,0 +1,33 @@
+package exercise3;
+
+import java.util.Arrays;
+
+import util.FillUp;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter 10 elements:");
+
+		int l = 10;
+		Integer[] nums = FillUp.arrayOfRandoms(l--);
+		
+		System.out.println("Initial array is " + Arrays.toString(nums));
+		
+		int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE, temp;
+		
+		for (int i = 0; i <= l; ++i) {
+			temp = nums[i];
+			nums[i] = nums[l-i];
+			nums[l-i] = temp;
+			
+			max = Math.max(nums[i], temp);
+			min = Math.min(nums[i], temp);
+		}
+		
+		System.out.println("Reversed array is " + Arrays.toString(nums));
+		System.out.println("Maximum of them is " + max);
+		System.out.println("Minimum of them is " + min);
+	}
+}
